2d66432d5aeadcd9bca892ad91ebdc95d66ecce0
jeff <jeff@sima.ai>
2021-07-26 13:48:57 -0700

    Add Builds Scripts for command line builds

---
+++ Assets/Scripts/Editor/Build.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class Build {
+  static string[] scenes = { 
+    "Assets/Karting/Scenes/IntroMenu.unity", 
+    "Assets/Karting/Scenes/MainScene.unity", 
+    "Assets/Karting/Scenes/LoseScene.unity", 
+    "Assets/Karting/Scenes/WinScene.unity" 
+  };
+
+  static void MacOs() {
+    BuildPipeline.BuildPlayer(scenes, "./Builds/game.osx", BuildTarget.StandaloneOSX, BuildOptions.None);      
+  }
+
+  static void Windows() {
+    BuildPipeline.BuildPlayer(scenes, "./Builds/game.x64", BuildTarget.StandaloneWindows64, BuildOptions.None);      
+  }
+}
